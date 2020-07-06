920851221a59b1ca8a669d2d0f46442fe4dd42f4
SLIPPERS <evansonoka05.j@gmail.com>
2020-07-06 20:50:36 +0300

    build GUI

--- Source/com/jadayoIscariot/MQTTDashboard.java
+++ Source/com/jadayoIscariot/MQTTDashboard.java
@@ -1,5 +1,11 @@
 package com.jadayoIscariot;
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 public class MQTTDashboard{
 	public static void main(String[] args){
 		System.out.println("MQTT is up");
